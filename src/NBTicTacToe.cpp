1de5e3d2ff84cc3325339203a52d031816cfec62
astroterm <henry@astrocore.dev>
2026-08-29 13:15:15 +1000

    THINK, WSU

--- src/NBTicTacToe.cpp
+++ src/NBTicTacToe.cpp
@@ -1,5 +1,16 @@
 #include "NBTicTacToe.hpp"
+
+#include <iostream>
+
 #include "TicTacToe.hpp"
+
+namespace {
+    bool sameTTT(BoardRef ref, TicTacToe* ttt) {
+        if (!ref) return false;
+        if (&ref.value().get() == ttt) return true;
+        return false;
+    }
+}
 
 NBTicTacToe::NBTicTacToe() :
     status {},
@@ -8,5 +19,13 @@ NBTicTacToe::NBTicTacToe() :
 {}
 
 void NBTicTacToe::displayBoards() const {
-    
+    for (const auto& row : nboard) {
+        for (int i = 0; i < BOARDSIZE; i++) {
+            for (const auto& col : row) {
+                col.displayRow(i, '|', '|');
+            }
+            std::cout << "\n";
+        }
+        std::cout << "\n";
+    }
 }
