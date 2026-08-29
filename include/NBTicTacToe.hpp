a598755d65bbbae939eaa05941a61555718d7d5e
astroterm <henry@astrocore.dev>
2026-08-29 18:34:55 +1000

    EVERY STUDENT AND EVERY TEACHER WILL BE GONE

--- include/NBTicTacToe.hpp
+++ include/NBTicTacToe.hpp
@@ -6,15 +6,16 @@
 #include <functional>
 #include <optional>
 
-
-using NBoard = std::array<std::array<TicTacToe, BOARDSIZE>, BOARDSIZE>;
-using BoardRef = std::optional<std::reference_wrapper<TicTacToe>>;
-
 enum class SelectBoardError {
     OutOfBounds,
     BoardFull,
     GameOver
 };
+
+using NBoard = std::array<std::array<TicTacToe, BOARDSIZE>, BOARDSIZE>;
+using BoardRef = std::optional<std::reference_wrapper<TicTacToe>>;
+using PlayError = std::expected<void, std::variant<SelectBoardError, MoveError>>;
+
 
 class NBTicTacToe {
 public:
@@ -25,7 +26,7 @@ public:
 private:
     void updateStatus();
     std::expected<void, SelectBoardError> selectBoard();
-    bool play(PlayerRef);
+    PlayError play(PlayerRef);
 
     NBoard nboard;
     BoardRef board;
